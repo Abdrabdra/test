001f8182731473bc451e28155b7b793fbb01e000
Abdrabdra <dico.bb15@mail.ru>
2023-03-06 22:27:16 +0600

    Dias

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ const Main = () => {
   return (
     <div>
       Main File
+      asdasd
       <div>asd</div>
       <div>asd</div>
       <div>asd</div>
@@ -12,8 +13,9 @@ const Main = () => {
       <div>asd</div>
       <div>asd</div>
       <div>asd</div>
-      
       <div>TestBranch</div>
+      <div>New Changes</div>
+      <div>Dias</div>
     </div>
   );
 };
